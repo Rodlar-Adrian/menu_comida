3832df6cf7626a4095b22e178995cdaa4b3f6917
omegazlp <omegazero2112@me.com>
2023-10-29 21:47:18 -0600

    Se agrego el icono para el nab 	Se agrego fuente para el nab     Se agrega el icono de whatsapp para la seccion contacto     se agrego codigo para movimiento entre menus     se agregaron los estilos para el nab 	se agrego el nab y las secciones contacto y ubicacion

---
+++ display.js
@@ -0,0 +1,24 @@
+let = menu = () => {
+    document.getElementById('obj1').style.display=''
+    document.getElementById('obj2').style.display='none'
+    document.getElementById('obj3').style.display='none'
+    document.getElementById('obj4').style.display='none'
+}
+let = bebidas = () => {
+    document.getElementById('obj1').style.display='none'
+    document.getElementById('obj2').style.display=''
+    document.getElementById('obj3').style.display='none'
+    document.getElementById('obj4').style.display='none'
+}
+let = contacto = () => {
+    document.getElementById('obj1').style.display='none'
+    document.getElementById('obj2').style.display='none'
+    document.getElementById('obj3').style.display=''
+    document.getElementById('obj4').style.display='none'
+}
+let = ubicacion = () => {
+    document.getElementById('obj1').style.display='none'
+    document.getElementById('obj2').style.display='none'
+    document.getElementById('obj3').style.display='none'
+    document.getElementById('obj4').style.display=''
+}
